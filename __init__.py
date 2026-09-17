9b2630f89bb859c4604d7e15ca36d8cac3846e0f
ash-r640-0 <phsilbert@gmail.com>
2026-09-17 23:27:20 +0000

    fix: remove resolved procedure export markers

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from .compiler import ProcedureCompileError, compile_procedure
 from .engine import ProcedureEngine, ProcedurePreflightError, ProcedureRunError
 from .invoker import ActionInvocation, ActionInvoker, Clock, EventSink, InputProvider, PollResult
 from .model import ActionRef, ActionResult, ParameterRef, Procedure, ProcedureSession, SessionState, Step, StepKind, StepRef, TypedRef
-<<<<<<< HEAD
 from .events import EvidenceStrength, EventRecorder, ObservedInvoker, ProcedureEvent, ProcedureEventStream, RequiredObserverError, redact_payload
 from .sinks import CheckpointDestination, INITIAL_SINK_IDS, MutationOutcome, SessionBinding, SinkEffect, SinkError, SinkRegistry, SinkRequest, SinkSpec
 __all__ = ["ActionInvocation", "ActionInvoker", "ActionRef", "ActionResult", "CheckpointDestination", "Clock", "EventSink", "EvidenceStrength", "EventRecorder", "INITIAL_SINK_IDS", "InputProvider", "MutationOutcome", "ObservedInvoker", "ParameterRef", "PollResult", "Procedure", "ProcedureCompileError", "ProcedureEngine", "ProcedureEvent", "ProcedureEventStream", "ProcedurePreflightError", "ProcedureRunError", "ProcedureSession", "RequiredObserverError", "SessionBinding", "SessionState", "SinkEffect", "SinkError", "SinkRegistry", "SinkRequest", "SinkSpec", "Step", "StepKind", "StepRef", "TypedRef", "compile_procedure", "redact_payload"]
